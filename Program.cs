60a6826511d6e7c969c70fed875edae770c1daae
Imbadimasa <chalganovdima@gmail.com>
2021-10-10 22:22:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Algorithms_Class_3
+{
+    class Program
+    {
+
+        public class Point
+        {
+            private double x, y;
+            public Point (double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public double X { get { return x; } private set { x = value; } }
+            public double Y { get { return y; } private set { y = value; } }
+
+        }
+
+        public struct GetDistance
+        {
+                public static double xDistance (double x1, double y1, double x2, double y2)
+            {
+                double dis = Math.Sqrt((x1 - x2)*(x1-x2) + ((y1 - y2) * (y1 - y2)));
+                return dis;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Point A = new Point(1, 4);
+            Point B = new Point(5, 10);
+
+            double result = GetDistance.xDistance(A.X, A.Y, B.X, B.Y);
+            Console.WriteLine(result);
+        }
+    }
+}
